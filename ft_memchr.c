6da6ba2249bd9d6e108559c10bfdf86e5c07037f
makarbass <52400119+makarbass@users.noreply.github.com>
2021-05-05 14:24:54 +0300

    Add files via upload

--- ft_memchr.c
+++ ft_memchr.c
@@ -5,42 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bpatrici <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/29 15:41:44 by bpatrici          #+#    #+#             */
-/*   Updated: 2021/04/29 15:54:20 by bpatrici         ###   ########.fr       */
+/*   Created: 2021/05/05 14:00:47 by bpatrici          #+#    #+#             */
+/*   Updated: 2021/05/05 14:23:41 by bpatrici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
 void *ft_memchr(const void *arr, int c, size_t n)
 {
-	int i;
-	size_t nk;
+	const char *a;
+	size_t i;
 
-	nk = 0;
+	a = arr;
 	i = 0;
-	while(nk < n)
+	
+	while (i < n)
 	{
-		if (arr[i] == c)
-		   return (c);
- 		else
-			i++;		
-	nk++;
+		if (a[i] == c)
+			return ((void *)a + i);
+		i++;
 	}
 	return 0;
-}
-
-int main()
-{
-	unsigned char src[15] = "1234567890";
-	unsigned char src2[15] = "1234567890";
-	char *sym;
-	char *sym2;
-
-	sym = memchr(src,'4', 10);
-	sym2 = ft_memchr(src,'4',10);
-
-	printf("%c\n", sym[0]);
-	printf("%c\n", sym2[0]);
 }
--- libft.h
+++ libft.h
@@ -19,6 +19,8 @@
 void ft_bzero(void *dest, size_t n);
 void *ft_memset(void *dest, int c, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
+void *ft_memccpy(void *dest, const void *src, int c, size_t n);
+void *ft_memchr(const void *arr, int c, size_t n);
 char *ft_strchr(const char *str, int ch);
 char *ft_strrchr(const char *str, int ch);
 
